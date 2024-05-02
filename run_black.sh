7b3877c6ac2593d436ec7db0f143ebb2f89625ca
Hari Adhikari <hadhikar@insight.com>
2024-05-01 21:43:21 -0400

    test

--- run_black.sh
+++ run_black.sh
@@ -2,7 +2,7 @@
 
 # run black command
 black .
-last_commit_message = $(git log -1 --pretty=format:"%s")
+last_commit_message=$(git log -1 --pretty=format:"%s")
 
 # check if there are any changes, if so add them to the commit
 if ! git diff --cached --exit-code --quiet; then
