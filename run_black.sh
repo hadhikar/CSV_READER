cad7f506fad1f71aeab9ebb2f850d1321eed1a6d
Hari Adhikari <hadhikar@insight.com>
2024-05-01 21:43:34 -0400

    test

--- run_black.sh
+++ run_black.sh
@@ -7,6 +7,6 @@ last_commit_message=$(git log -1 --pretty=format:"%s")
 # check if there are any changes, if so add them to the commit
 if ! git diff --cached --exit-code --quiet; then
     git add -u
-else:
+else
     echo "No changes to commit"
 fi
